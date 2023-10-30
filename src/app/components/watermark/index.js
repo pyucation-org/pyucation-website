71569b16af6c85e41abd583f461d331da6664ba2
batuguel <gulbatu3@googlemail.com>
2023-10-30 18:49:37 +0100

    change watermark

--- src/app/components/watermark/index.js
+++ src/app/components/watermark/index.js
@@ -6,17 +6,17 @@ function Watermark() {
   return (
     <Link href="/">
       <div className="flex flex-row items-center justify-center">
-        <div className="hidden xl:flex text-md text-zinc-200 tracking-widest">
-          pyucation
-        </div>
         <Image
-          className="right-4 min-w-[40px]"
+          className="min-w-[40px]"
           src="/logo_notext_white.svg"
           alt="Next.js Logo"
           width={35}
           height={35}
           priority
         />
+        <div className="hidden xl:flex text-md text-zinc-200 tracking-widest">
+          pyucation
+        </div>
       </div>
     </Link>
   );
