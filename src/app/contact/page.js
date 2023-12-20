37f128e65446f2d95cd7aa91983cf7ccba138998
Soxxes <marc_anmeldungen@proton.me>
2023-12-20 17:38:26 +0100

    minor adjustments contact section

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -26,12 +26,12 @@ export default function Page() {
           </div>
           <div>
           <label for="surname" className="block mb-2 text-sm font-medium text-zinc-400">Nachname</label>
-          <input type="text" id="surname" name="surname" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Muster" required></input>
+          <input type="text" id="surname" name="surname" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Mustermann" required></input>
           </div>
         </div>
           <div>
               <label for="email" className="block mb-2 text-sm font-medium  text-zinc-400">Email</label>
-              <input type="email" id="email" name="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="example@pyucation.com" required/>
+              <input type="email" id="email" name="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="example@pyucation.de" required/>
           </div>
           <div>
               <label for="phone" class="block mb-2 text-sm font-medium text-zinc-400">Telefonnummer</label>
@@ -41,7 +41,7 @@ export default function Page() {
               <label for="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Ihre Nachricht</label>
               <textarea id="message" rows="6" name="message" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Beschreiben Sie Ihr Anliegen"></textarea>
           </div>
-          <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit focus:ring-4 focus:outline-none focus:ring-secondary bg-secondary hover:bg-primary">Send message</button>
+          <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit focus:ring-4 focus:outline-none focus:ring-secondary bg-secondary hover:bg-primary">Absenden</button>
       </form>
   </div>
     </motion.div>
