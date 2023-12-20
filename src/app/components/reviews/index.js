7b2800f7957853162d478c8e1788d03d8ac2f33b
Soxxes <marc_anmeldungen@proton.me>
2023-12-20 17:36:17 +0100

    decreased margin to review section

--- src/app/components/reviews/index.js
+++ src/app/components/reviews/index.js
@@ -65,7 +65,7 @@ function Reviews() {
 
   return (
     <motion.div className="flex flex-col h-auto max-w-6xl justify-start items-start mx-auto mt-56 pb-20" variants={fadeInVariant} initial="hidden" whileInView="visible">
-      <h1 className="text-white text-6xl font-semibold align-start leading-snug mt-20">
+      <h1 className="text-white text-6xl font-semibold align-start leading-snug mt-5">
         Wir sind stolz auf 100% Kundenzufriedenheit:
       </h1>
       <div className="flex flex-wrap justify-start items-center w-full mt-28 gap-4">
