289cdb49d730f1f0bae8d0c3e8619e2eb99e1d79
Batu <gulbatu3@googlemail.com>
2024-01-31 18:17:48 +0100

    add little transition animation

--- src/app/components/privacyModal/index.js
+++ src/app/components/privacyModal/index.js
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import { fadeInVariantDelay } from "@/app/util/variants";
+import { AnimatePresence, motion } from "framer-motion";
+import React, { useEffect, useState } from "react";
 
 const PrivacyModal = () => {
   const [showModal, setShowModal] = useState(false);
@@ -15,21 +17,33 @@ const PrivacyModal = () => {
     // Save consent in localStorage
     localStorage.setItem("privacyConsentGiven", "true");
     setShowModal(false);
-  }
-
-  if (!showModal) {
-    return null;
-  }
+  };
 
   return (
-    <div className="modal">
-      <div className="modal-content">
-        <h2>Datenschutzbestimmungen</h2>
-        <p>Datenschutz ist uns ein Anliegen. Wenn Sie diese Seite nutzen, stimmen Sie unseren <a href="/privacy-policy">Datenschutzrichtlinien</a> zu. Ihre Zustimmung wird gespeichert, es sei denn Sie nutzen eine private Browser Session.</p>
-        <button onClick={handleClose}>Ich stimme zu.</button>
-      </div>
-    </div>
+    <AnimatePresence>
+      {showModal && (
+        <motion.div
+          className="modal"
+          variants={fadeInVariantDelay}
+          initial="hidden"
+          animate="visible"
+          exit={{ opacity: 0 }}
+        >
+          <div className="modal-content">
+            <h2>Datenschutzbestimmungen</h2>
+            <p>
+              Datenschutz ist uns ein Anliegen. Wenn Sie diese Seite nutzen,
+              stimmen Sie unseren{" "}
+              <a href="/privacy-policy">Datenschutzrichtlinien</a> zu. Ihre
+              Zustimmung wird gespeichert, es sei denn Sie nutzen eine private
+              Browser Session.
+            </p>
+            <button onClick={handleClose}>Ich stimme zu.</button>
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
-}
+};
 
 export default PrivacyModal;
