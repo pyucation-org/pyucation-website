7c863df25469f4b0d5c8b62070d425850a97dc19
Soxxes <hscschwimmermarc@t-online.de>
2024-03-12 19:15:25 +0100

    made tiles responsive

--- src/app/components/vision/offering-card.js
+++ src/app/components/vision/offering-card.js
@@ -4,7 +4,7 @@ function Offering({ iconClass, description }) {
   return (
     <div className="offering bg-white bg-opacity-75 shadow-lg shadow-gray-500 backdrop-blur-xl rounded-xl flex flex-col items-center p-4 m-4">
       <i className={`${iconClass} custom-icon text-5xl mb-4`}></i>
-      <p className="text-center">{description}</p>
+      <p className="text-center text-sm md:text-base">{description}</p>
     </div>
   );
 }
